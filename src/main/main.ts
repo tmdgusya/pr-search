07a5a3cf565ac804397dd86fae0210e0d6081fc4
tmdgusya <tmdgusya@naver.com>
2022-06-06 00:08:30 +0900

    setting IOC Container

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,13 +1,38 @@
 import express from 'express';
 import {CODE_SQUAD, PR_TITLE,} from "./title";
+import {Server,} from "typescript-rest";
+import BodyParser from "body-parser"
+import cookieParser from "cookie-parser";
 
-const app = express();
+const app: express.Application = express();
 
-function init() {
-  app.listen(() => {
-    console.log(CODE_SQUAD)
-    console.log(PR_TITLE)
+class IOCContainer {
+
+  static async bindServer() {
+    Server.buildServices(app)
+  }
+
+  static async resolveDependencies() {
+    Server.loadServices(app, ['../main/controller/**/*.js',], __dirname)
+  }
+
+}
+
+async function init() {
+
+  app.use(cookieParser())
+  app.use(BodyParser.json())
+  app.use(BodyParser.urlencoded())
+
+  await IOCContainer.resolveDependencies()
+  await IOCContainer.bindServer()
+
+  app.listen(3000, () => {
+    console.log("BootStrap Sever!!")
   })
 }
 
-init();
+init().then(() => {
+  console.log(CODE_SQUAD)
+  console.log(PR_TITLE)
+});

cf42722c8e2edda2548d2d919f399818c2ff967e
tmdgusya <tmdgusya@naver.com>
2022-06-05 22:15:46 +0900

    add Title CodeSquad PullRequest
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣶⣿⣿⣿⣿⣶⣦⡀⢀⠠⢤⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣄⡀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣷⡟⢈⣠⠄⠙⣎⠳⣄⣠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⢿⣿⡍⠙⠛⠛⠿⠿⠛⠛⠋⢩⣿⡿⠏⠀⠀⣿⣄⠈⠙⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣷⡈⠙⠛⠀⠀⠛⠋⢁⣾⣿⣿⠀⠀⠀⠘⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣸⣿⣿⣿⣿⣿⣶⣶⣶⣶⣿⣿⣿⣿⣿⣇⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀
    ⠀⠀⠀⣠⠞⣉⡈⢩⡿⣏⣀⠀⣻⡏⢀⣀⠈⣿⡇⢠⣤⣤⡟⢁⣤⣈⣿⡟⢠⣄⠙⣿⠀⣿⣷⠈⣿⣿⠀⠹⣿⣧⠁⣀⡉⠳⣄⠀⠀⠀
    ⠀⠀⣼⠃⣴⣿⣷⡟⢠⣿⡟⢠⡿⠀⣿⡿⠀⣿⠀⠈⠉⢹⣧⣀⠍⠙⢿⡆⢸⣿⡆⢸⡇⠸⣿⡆⠸⣿⡤⢠⡘⢿⣧⠘⣿⣆⠹⣆⠀⠀
    ⠀⣼⠃⣼⡿⢿⡟⠀⣾⡟⢀⣾⠃⣸⡿⠃⣸⡏⢠⣿⡿⣿⡿⠿⡿⠇⢸⣇⠘⢏⠁⢈⣷⡀⢻⣿⠀⢻⡇⠈⠛⠈⢻⣆⠘⣿⣆⠘⣆⠀
    ⢸⡇⠀⠛⢁⣼⡇⠀⢉⣠⢾⣏⣀⣀⣠⠼⣻⣃⣤⣤⣤⣿⠦⣤⣤⠴⠟⠙⠦⣤⣤⣄⣽⠳⣤⣀⣀⡾⣇⡀⣶⠲⣄⠹⣆⠘⠛⠀⢸⡆
    ⠘⢧⡤⠞⠋⠀⠛⠛⠋⠁⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠀⠀⠈⠁⠀⠉⠉⠙⠀⠙⠛⠻⠶⠦⠴⠾⠃
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⣴⠶⢦⡄⠀⠀⠀⠀⢰⡄⣶⠀⠀⢰⡶⠶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⢰⠀⠀⠀⠀
    ⠀⠀⣿⣀⣠⡏⢸⠀⢸⡇⢸⡇⣿⠀⠀⢸⣇⣠⡿⢠⣞⣛⣦⢠⡞⠓⣿⠀⡇⠀⣿⢠⣞⣛⣦⢰⣟⠛⠀⢻⡟⢰⣟⠛⠂⢸⠀⠀⠀⠀
    ⠀⠀⣿⠉⠁⠀⢸⣄⣸⡇⢸⡇⣿⠀⠀⢸⡇⠘⣧⠘⣯⣉⡥⠸⣧⣠⣿⠀⣧⣠⣿⠘⣯⣉⡭⠠⣍⣹⠆⢸⣇⠠⣍⣹⠆⢨⡀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import express, { Request, Response, NextFunction } from 'express';
-import {CODE_SQUAD} from "./title";
+import express from 'express';
+import {CODE_SQUAD, PR_TITLE,} from "./title";
 
 const app = express();
 
 function init() {
   app.listen(() => {
     console.log(CODE_SQUAD)
+    console.log(PR_TITLE)
   })
 }
 
